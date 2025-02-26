8111217e58bdcb564f3c83cc4a6bdfc654b1f2b9
loveCiForever <quanghuy71847@gmail.com>
2025-02-26 13:06:31 +0700

    setup frontend side (client)

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <p className="text-6xl font-semibold">Hello World!</p>
+    </div>
+  );
+}
+
+export default App;
