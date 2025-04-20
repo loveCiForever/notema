d51e0a34ea335a03fcd86367a6d67225905bc652
Quang Huy (Victor) Nguyen <107240800+loveCiForever@users.noreply.github.com>
2025-04-20 19:16:43 +0700

    Delete client directory

--- client/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="flex justify-center items-center w-full h-screen">
-      <p className="text-6xl font-semibold">Hello World!</p>
-    </div>
-  );
-}
-
-export default App;
