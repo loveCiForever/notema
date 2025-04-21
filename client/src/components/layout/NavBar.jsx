40b0bb95031cea8ba6bb90546eaddc2864c20ba3
loveCiForever <quanghuy71847@gmail.com>
2025-04-21 16:24:27 +0700

    add light and dark theme

--- client/src/components/layout/NavBar.jsx
+++ client/src/components/layout/NavBar.jsx
@@ -1,41 +1,108 @@
 import logo from "../../assets/logo.svg";
 import { useNavigate } from "react-router-dom";
+import { useTheme } from "../../contexts/ThemeContext";
 
-const NavBar = () => {
+const NavBar = ({}) => {
   const navigate = useNavigate();
+  const { theme, toggleTheme } = useTheme();
+  const currentLocation = window.location.pathname;
+  const isLandingPage = currentLocation === "/";
 
   return (
-    <div className="flex fixed h-16 items-center justify-between px-4 lg:px-10 py-2 bg-transparent// backdrop-blur-md w-full text-black  bg-red-100//">
-      <button
-        className="flex"
-        onClick={() => {
-          window.location.reload();
-        }}
-      >
-        {/* <img src={logo} alt="" className="w-10" /> */}
-        <h1 className="text-xl font-bold">Notema</h1>
-      </button>
-
-      <div className="flex items-center gap-4 text-md font-medium bg-red-100//">
-        <button
-          className="px-4 py-1 rounded-md hover:bg-gray-100"
-          onClick={() => {
-            navigate("/login");
-          }}
+    <div>
+      {isLandingPage ? (
+        <div
+          className={`flex fixed h-16 items-center justify-between bg-transparent px-4 lg:px-10 py-2 backdrop-blur-md w-full text-black`}
         >
-          Login
-        </button>
+          <button
+            className="flex"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            {/* <img src={logo} alt="" className="w-10" /> */}
+            <h1 className="text-xl font-bold">Notema.</h1>
+          </button>
 
-        <div className="h-5 border-[1px]"></div>
-        <button
-          className="px-4 py-1 rounded-md hover:bg-gray-100"
-          onClick={() => {
-            navigate("/register");
-          }}
+          <div className="flex items-center gap-4 text-md font-medium">
+            <button
+              className={`px-4 py-1.5 rounded-md hover:bg-gray-200`}
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </button>
+
+            <div className="h-5 border-[1px]"></div>
+            <button
+              className={`px-4 py-1.5 rounded-md hover:bg-gray-200`}
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div
+          className={`flex fixed h-16 items-center justify-between bg-transparent px-4 lg:px-10 py-2 backdrop-blur-md w-full text-black ${
+            theme === "dark" ? "text-white bg-black/60" : ""
+          }`}
         >
-          Register
-        </button>
-      </div>
+          <button
+            className="flex"
+            onClick={() => {
+              window.location.reload();
+            }}
+          >
+            {/* <img src={logo} alt="" className="w-10" /> */}
+            <h1 className="text-xl font-bold">Notema.</h1>
+          </button>
+
+          <div className="flex items-center gap-4 text-md font-medium bg-red-100//">
+            <div>
+              <button
+                className={`text-sm px-5 py-1.5 mr-4 rounded-lg  tracking-wider ${
+                  theme === "dark"
+                    ? "bg-white text-black hover:bg-gray-300"
+                    : "bg-black/83 hover:bg-black/60 text-white"
+                }`}
+                onClick={toggleTheme}
+              >
+                Switch to {theme == "light" ? "Dark mode" : "Light mode"}
+              </button>
+            </div>
+            <button
+              className={`px-4 py-1.5 rounded-md ${
+                theme === "dark"
+                  ? " text-white hover:bg-black/50"
+                  : "text-black hover:bg-gray-100"
+              }`}
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              Login
+            </button>
+
+            <div className="h-5 border-[1px]"></div>
+            <button
+              className={`px-4 py-1.5 rounded-md ${
+                theme === "dark"
+                  ? " text-white hover:bg-black/50"
+                  : "text-black hover:bg-gray-100"
+              }`}
+              onClick={() => {
+                navigate("/register");
+              }}
+            >
+              Register
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import NavBar from "../components/layout/NavBar";
+import { useTheme } from "../contexts/ThemeContext";
 
 const HomePage = () => {
+  const { theme } = useTheme();
   return (
-    <div className="w-full h-screen items-center justify-center">
+    <div
+      className={`w-full h-screen items-center justify-center ${
+        theme == "dark" ? "bg-black/90" : "bg-white"
+      }`}
+    >
       <NavBar />
     </div>
   );
