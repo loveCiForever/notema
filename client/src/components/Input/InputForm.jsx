5a7b6624f5deefb1b35e9570a7e8f5efe4ea0a8f
loveCiForever <quanghuy71847@gmail.com>
2025-04-27 22:51:19 +0700

    UI:fix responsive of login page for all screen size, add second login method button (github auth)

--- client/src/components/Input/InputForm.jsx
+++ client/src/components/Input/InputForm.jsx
@@ -1,17 +1,17 @@
-const InputForm = ({ icon, placeholder,value,type,onChange}) => {
+const InputForm = ({ icon, placeholder, value, type, onChange }) => {
   return (
-    <div className="flex flex-col gap-y-2">
-      <div className="flex items-center">
-        <div className="pl-2 absolute z-10 text-black"><img src={icon} alt={icon} /></div>
-        <input
-          className="relative pl-9 border-[2px] border-black outline-none px-10 py-2 rounded-lg hover:bg-gray-100  hover:border-blue-600  focus:bg-gray-100 focus:border-blue-600 "
-          type={type}
-          name="username"
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-        />
+    <div className="flex w-full border-[1px] border-black/10 items-center justify-center rounded-lg">
+      <div className="pl-4 text-black">
+        <img src={icon} alt={"input-form-icon"} />
       </div>
+      <input
+        className="ml-4 pl-4 outline-none w-full py-3  hover:border-blue-600  focus:border-blue-600 focus:bg-gray-100"
+        type={type}
+        name="username"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
     </div>
   );
 };
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,57 +1,75 @@
-import videofram_login from "../assets/videoframe_login.png";
+import video_frame_login from "../assets/videoframe_login.png";
 import InputForm from "../components/Input/InputForm";
 import email from "../assets/svg/email.svg";
 import lock from "../assets/svg/lock.svg";
 import logo from "../assets/logo/logo.png";
-import gmail from "../assets/gmail.png";
+import GoogleLogo from "../assets/logo/googleLogo.svg";
+import GithubLogo from "../assets/logo/githubLogo.svg";
+import { useNavigate } from "react-router-dom";
+
 const LoginPage = () => {
   const handleSubmit = () => {};
+  const navigate = useNavigate();
+
   return (
     <>
-      <div className=" center h-screen flex items-center justify-center">
-        <div className=" rounded-3xl overflow-hidden shadow-lg">
-          <div className="relative grid grid-cols-2 max-[900px]:grid-cols-1 gap-4 w-full max-w-4xl h-auto">
-            <div className="flex justify-center items-center p-4 max-[900px]:hidden">
+      <div className="bg-gray-100 center h-screen flex items-center justify-center">
+        <div className="rounded-xl overflow-hidden shadow-md bg-white p-10 w-full mx-10 md:mx-40 lg:py-20 xl:py-10 lg:mx-60 xl:w-auto xl:min-w-[1000px]">
+          <div className="grid xl:grid-cols-2 gap-4 w-full max-w-4xl h-auto">
+            <div className="justify-center items-center p-4 hidden xl:block">
               <img
                 className="w-full h-auto object-cover"
-                src={videofram_login}
+                src={video_frame_login}
                 alt="Video Frame"
               />
             </div>
-            <form
-              onSubmit={handleSubmit}
-              className="flex flex-col justify-center items-center p-4 px-[90px] gap-3"
-            >
-              <h1 className="font-bold text-[30px]">Log in</h1>
+            <div className="flex flex-col justify-center items-center lg:px-[60px] gap-3 bg-red-100//">
+              <div className="flex w-full mb-6 items-center justify-start">
+                <img src={logo} alt="logo" className="w-[150px]" />
+              </div>
               <InputForm placeholder={"Email"} icon={email} type={"email"} />
               <InputForm
                 placeholder={"Password"}
                 icon={lock}
                 type={"password"}
               />
-              <p className="cursor-pointer text-[12px] w-full text-right text-gray-500 hover:text-blue-600">
+              <p className="cursor-pointer text-[14px] w-full text-right text-gray-500 hover:text-blue-600 active:scale-[.98] active:duration-75 transition-all">
                 Forgot password?
               </p>
               <button
-                type="submit"
-                className="bg-black text-white w-full py-2 rounded-lg cursor-pointer hover:bg-gray-800"
+                onClick={handleSubmit()}
+                className="bg-black text-lg font-bold text-white w-full py-2 rounded-xl cursor-pointer hover:bg-black/50 active:scale-[.98] active:duration-75 transition-all"
               >
                 Log in
               </button>
               <div className="flex items-center w-full my-2">
-                <div className="flex-1 h-px bg-gray-500"></div>
-                <p className="px-4 text-[12px] text-gray-500 whitespace-nowrap ">
+                <div className="flex-1 h-px bg-gray-400"></div>
+                <p className="px-4 text-[14px] text-gray-500 whitespace-nowrap ">
                   Or log in with
                 </p>
-                <div className="flex-1 h-px bg-gray-500"></div>
+                <div className="flex-1 h-px bg-gray-400"></div>
               </div>
-              <button className="bg-gray-200 w-full py-2 rounded-lg cursor-pointer hover:bg-gray-300 center gap-2 ">
-                <img className="w-8" src={gmail} alt="gmail.png" />
-                <h>Log in with Google</h>
-              </button>
-            </form>
-            <div className="w-[150px] absolute top-3 right-1/6 max-[900px]:hidden">
-              <img src={logo} alt="logo" />
+              <div className="w-full flex gap-2">
+                <button className="bg-white border-[1px] border-gray-200 w-full py-3 rounded-xl cursor-pointer hover:bg-gray-100 center gap-4 active:scale-[.98] active:duration-75 transition-all">
+                  <img className="w-6" src={GoogleLogo} alt="gmail.png" />
+                  <h>Google</h>
+                </button>
+                <button className="bg-white border-[1px] border-gray-200 w-full py-3 rounded-xl cursor-pointer hover:bg-gray-100 center gap-4 active:scale-[.98] active:duration-75 transition-all">
+                  <img className="w-6" src={GithubLogo} alt="github.png" />
+                  <h>Github</h>
+                </button>
+              </div>
+              <div className="flex items-center justify-center mt-4 gap-1">
+                <p className="text-[14px] text-gray-800">
+                  Don't have an account ?
+                </p>
+                <button
+                  onClick={() => navigate("/register")}
+                  className="ml-1 text-[14px] font-semibold text-black hover:underline active:scale-[.98] active:duration-75 transition-all"
+                >
+                  {"   Register one"}
+                </button>
+              </div>
             </div>
           </div>
         </div>
