680ea2fc15275d90ce0252a5943b4cd3451108b6
loveCiForever <quanghuy71847@gmail.com>
2025-04-27 12:51:37 +0700

    Footer: change bg-color, layout. Landingpage: remove unneccessary part

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,21 +1,21 @@
-import  { useState } from "react";
+import { useState } from "react";
 import logo from "../../assets/logo/logo.png";
 import menu from "../../assets/svg/menu.svg";
 import close from "../../assets/svg/close.svg";
 
 const Header = () => {
-  const [isClickMenu,setIsClickMenu] = useState("false")
-  console
+  const [isClickMenu, setIsClickMenu] = useState("false");
+  console;
   return (
     <>
-      <header className="h-[60px] bg-white">
-        <div className="flex w-full h-full px-4 ">
+      <header className="h-[70px] bg-white">
+        <div className="flex w-full h-full px-6 ">
           <div className="flex items-center w-full max-lg:justify-between ">
             <div className="w-[100px] cursor-pointer">
               <img src={logo} alt="logo" />
             </div>
-            <div className="flex justify-between w-full pl-4 max-lg:hidden ">
-              <div>
+            <div className="flex justify-between w-full pl-6 max-lg:hidden ">
+              <div className="flex gap-2">
                 <button className="btn-hover-gray">Product</button>
                 <button className="btn-hover-gray">Team</button>
                 <button className="btn-hover-gray">Individuals</button>
@@ -25,22 +25,39 @@ const Header = () => {
                 <button className="btn-black">Sign up</button>
               </div>
             </div>
-            <div className="right-0 hidden cursor-pointer max-lg:block"onClick={()=>setIsClickMenu(!isClickMenu)}>
-              {!isClickMenu ?<img src={close} alt="icon-close" /> :<img src={menu} alt="icon-menu" />}
+            <div
+              className="right-0 hidden cursor-pointer max-lg:block"
+              onClick={() => setIsClickMenu(!isClickMenu)}
+            >
+              {!isClickMenu ? (
+                <img src={close} alt="icon-close" />
+              ) : (
+                <img src={menu} alt="icon-menu" />
+              )}
             </div>
           </div>
         </div>
-        <div className={ `hidden absolute w-[200px] h-[240px] right-0 rounded-b-2xl bg-white z-10 ${isClickMenu ? "hidden " : "max-lg:block"}  `}>
-        <div className=" px-4 font-bold ">
-          <div className="py-2 border-gray-300 border-y-1 cursor-pointer hover:bg-gray-100">Product</div>
-          <div className="py-2 border-gray-300 border-y-1  cursor-pointer hover:bg-gray-100">Team</div> 
-          <div className="py-2 border-gray-300 border-y-1  cursor-pointer hover:bg-gray-100">Individuals</div>
+        <div
+          className={`hidden absolute w-[200px] h-[240px] right-0 rounded-b-2xl bg-white z-10 ${
+            isClickMenu ? "hidden " : "max-lg:block"
+          }  `}
+        >
+          <div className="px-4 font-bold ">
+            <div className="py-2 border-gray-300 cursor-pointer border-y-1 hover:bg-gray-100">
+              Product
+            </div>
+            <div className="py-2 border-gray-300 cursor-pointer border-y-1 hover:bg-gray-100">
+              Team
+            </div>
+            <div className="py-2 border-gray-300 cursor-pointer border-y-1 hover:bg-gray-100">
+              Individuals
+            </div>
+          </div>
+          <div className="flex flex-col w-full gap-2 px-4 pt-4 bg-white border-gray-300 ">
+            <button className="text-center btn-black">Log in</button>
+            <button className="text-center btn-black-outline">Sign up</button>
+          </div>
         </div>
-        <div className=" pt-4 flex flex-col px-4 w-full  border-gray-300 bg-white gap-2">
-          <button className=" btn-black text-center">Log in</button>
-          <button className=" btn-black-outline text-center">Sign up</button>
-        </div>
-      </div>
       </header>
     </>
   );
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -2,34 +2,10 @@ import BackToTop from "../components/button/BackToTop";
 import Footer from "../components/layout/Footer";
 import Header from "../components/layout/Header";
 import { useEffect, useRef, useState } from "react";
-import "../Style/transition.css";
+import LandingPageImage1 from "../assets/videoframe_0.png";
+import LandingPageImage2 from "../assets/videoframe_2.png";
+
 const LandingPage = () => {
-  const imagesfake = [];
-  const [currentIndex, setCurrentIndex] = useState(0);
-  const timeoutRef = useRef(null);
-
-  const delay = 3000;
-
-  const resetTimeout = () => {
-    if (timeoutRef.current) {
-      clearTimeout(timeoutRef.current);
-    }
-  };
-
-  useEffect(() => {
-    resetTimeout();
-    timeoutRef.current = setTimeout(
-      () =>
-        setCurrentIndex((prevIndex) =>
-          prevIndex === imagesfake.length - 1 ? 0 : prevIndex + 1
-        ),
-      delay
-    );
-    return () => {
-      resetTimeout();
-    };
-  }, [currentIndex]);
-
   const [isScrolled, setIsScrolled] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
@@ -42,76 +18,53 @@ const LandingPage = () => {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return (
     <>
       <div
-        className={` ${
+        className={`${
           isScrolled ? "shadow-2xl" : ""
         } relative sm:fixed w-full top-0 z-10 `}
       >
         <Header />
       </div>
-      <div
-        className="relative flex bg-white overflow-hidden"
-        style={{ height: "calc(100vh - 60px)" }}
-      >
-        {imagesfake.map((src, index) => (
-          <img
-            key={index}
-            src={src}
-            alt={`slide-${index}`}
-            className={`absolute w-full h-screen object-cover transition-opacity duration-1000 ease-in-out ${
-              index === currentIndex ? "opacity-100" : "opacity-0"
-            }`}
-          />
-        ))}
-      </div>
-      <div className="px-20 py-10 flex flex-col">
-        <div className="grid grid-cols-2 max-lg:grid-cols-1 gap-4 h-screen items-center">
-          <div className="flex flex-col gap-5">
-            <h1 className="text-[76px] font-bold leading-[73px]">
+      <div className="flex flex-col mt-0 px-60 ">
+        <div className="flex items-center w-full h-screen grid-cols-2 gap-4 bg-red-200// content-1 max-lg:grid-cols-1 ">
+          <div className="flex flex-col gap-8">
+            <h1 className="text-[80px] font-bold leading-[73px]">
               The convenient working space
             </h1>
-            <p className="text-[22px] leading-[28px]">
+            <p className="text-[25px] leading-[30px] text-black/70">
               Are you afraid of forgetting important tasks? Create your plan
               smartly and get reminders whenever you forget.
             </p>
             <div className="flex gap-3">
-              <button className="btn-primary px-4 py-2 font-bold rounded-lg ">
+              <button className="px-4 py-2 font-bold rounded-lg btn-primary ">
                 Sign up
               </button>
-              <button className=" btn-primary-outline px-4 py-2 font-bold rounded-lg  ">
+              <button className="px-4 py-2 font-bold rounded-lg btn-primary-outline">
                 Log in
               </button>
             </div>
           </div>
-          <div>
-            <video
-              className="fix-img"
-              src="https://www.notion.com/front-static/pages/product/super-duper/hero/homepage-hero.mp4"
-            ></video>
-          </div>
+          <img src={LandingPageImage1} alt="" className="w-1/2" />
         </div>
-        <div className="grid grid-cols-2 max-xl:grid-cols-1 gap-4 h-screen items-center">
+        <div className="flex items-center w-full h-screen grid-cols-2 gap-4 bg-green-200// content-2 max-xl:grid-cols-1 ">
           <div className="flex flex-col gap-5">
-            <h1 className="text-[60px] font-bold leading-[64px] ">
+            <h1 className="text-[70px] font-bold leading-[73px] ">
               Share your planning ideas with your friends.
             </h1>
-            <p className="text-[18px] leading-[24px] text-[#78736f]">
+            <p className="text-[20px] leading-[24px] text-black/70">
               Your friends can view your plans, interact with you, and make life
               more community-oriented.
             </p>
           </div>
-          <div>
-            <video
-              className="fix-img"
-              src="https://www.notion.com/front-static/pages/product/super-duper/write/write-screen-desktop.mp4"
-            ></video>
-          </div>
+
+          <img src={LandingPageImage2} alt="" className="w-1/2" />
         </div>
       </div>
-      <BackToTop></BackToTop>
-      <Footer></Footer>
+      <BackToTop />
+      <Footer />
     </>
   );
 };
